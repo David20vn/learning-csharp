2b601ac593321c3b08605435d2faae895e156f4f
david <david20vn@gmail.com>
2026-02-17 18:50:35 -0500

    Data types

--- toLearn.cs
+++ toLearn.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 
 class Program {
-  static void Main( string[] args ){
-    Console.WriteLine( "Hello world!");
+  static void Main( string[] args){
+    string num = "349";
+    short numParsed;
+    bool succed = short.TryParse( num, out numParsed );
+    Console.WriteLine ( ++numParsed );
+    Console.WriteLine ( numParsed );
   }
-}
+} 
