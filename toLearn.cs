f8a107fd2601bf6e48975da3385d57c0d4077340
david <david20vn@gmail.com>
2026-02-19 17:36:52 -0500

    Inputs and outputs

--- toLearn.cs
+++ toLearn.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/* simple
 
 class Program {
   static void Main( string[] args){
@@ -14,3 +15,48 @@ class Program {
     Console.WriteLine ( numParsed );
   }
 } 
+
+*/
+
+/* simple operation
+
+class Program {
+  static void Main( string[] args){
+
+    const float discount = 20f;
+
+    float totalPrice;
+
+    if ( float.TryParse (Console.ReadLine(), out totalPrice) ){
+      Console.WriteLine( totalPrice*(1-(discount/100)) );
+    }
+
+  }
+} 
+
+*/
+
+/* Simple input output
+
+class Program {
+  static void Main( string[] args ){
+
+    string name;
+    long phoneNumber;
+    byte age;
+
+    Console.Write( "Write your name: ");
+    name = Console.ReadLine();
+
+    Console.Write("Write your phone number: ");
+    long.TryParse( Console.ReadLine(), out phoneNumber );
+    
+    Console.Write("Write your age: ");
+    byte.TryParse( Console.ReadLine(), out age );
+
+    Console.WriteLine($"\n{name}.\n{phoneNumber}.\n{age}.");
+
+  }
+}
+
+*/
